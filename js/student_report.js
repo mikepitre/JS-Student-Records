e7f554fc93d5816a359f7c5fa0eae92016c1d1de
Mike Pitre <michaelepitre@gmail.com>
2015-09-01 11:05:47 -0500

    fixed script link typo, added student props to output

--- js/student_report.js
+++ js/student_report.js
@@ -1,7 +1,17 @@
 var message = '';
 var student;
 
-function print (message) {
+function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 };
+
+for (var i = 0; i < students.length; i++) {
+  student = students[i]
+  message += '<h2>Student: ' + student.name + '</h2>';
+  message += '<p>Track: ' + student.track + '</p>';
+  message += '<p>Achievements: ' + student.achievements + '</p>';
+  message += '<p>Points: ' + student.points + '</p>';
+};
+
+print(message);
