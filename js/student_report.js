ca27ceb57f0a74f776e9adc69298b42295c14ff0
Mike Pitre <michaelepitre@gmail.com>
2015-09-01 11:30:59 -0500

    created getStudentReport function

--- js/student_report.js
+++ js/student_report.js
@@ -5,21 +5,29 @@ var search;
 function print(message) {
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
-};
+}
 
+function getStudentReport(student) {
+  var report = '<h2>Student: ' + student.name + '</h2>';
+  report += '<p>Track: ' + student.track + '</p>';
+  report += '<p>Achievements: ' + student.achievements + '</p>';
+  report += '<p>Points: ' + student.points + '</p>';
+  return report;
+}
 
 while (true) {
   search = prompt("Enter a student's name to search for their info. You can also type 'quit' to exit the search.");
-  if (search.toLowerCase() === 'quit') {
+  if (search === null || search.toLowerCase() === 'quit') {
     break;
+  }
+  for (var i = 0; i < students.length; i++) {
+    student = students[i]
+    if (student.name === search) {
+      message = getStudentReport(student);
+      print(message);
+    }
   }
 }
 
 
-for (var i = 0; i < students.length; i++) {
-  student = students[i]
-  message += '<h2>Student: ' + student.name + '</h2>';
-  message += '<p>Track: ' + student.track + '</p>';
-  message += '<p>Achievements: ' + student.achievements + '</p>';
-  message += '<p>Points: ' + student.points + '</p>';
-};
+
